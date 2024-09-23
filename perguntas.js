a12741e700d4a94cd05d92bbbe45b191f93ed08a
pesssoacomdepressao <146104216+pesssoacomdepressao@users.noreply.github.com>
2024-09-23 08:32:27 -0300

    4,5 e 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'História',
+    'Em que ano a Segunda Guerra Mundial começou??',
+    'No ano de 1939'
+)
+
+criaCartao(
+    'Português',
+    'Quem escreveu Dom Casmurro??',
+    'Foi o Machado de Assis'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
